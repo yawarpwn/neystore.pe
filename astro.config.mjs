ee5b2889b9b8ace2a9d6ace334ef3d267097e3b5
johneyder <john.kazz@gmail.com>
2024-05-01 10:10:16 -0500

    initial config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://neystore.vercel.app',
+  base: '/',
+  trailingSlash: 'never',
+  output: 'static',
+  compressHTML: true,
+  scopedStyleStrategy: 'attribute',
+  build: {
+    assets: '_astro',
+  },
+  integrations: [tailwind()]
+});
