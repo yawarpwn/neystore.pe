d9c9184adffa6e9acac81819f01133e4ce4d6834
johneyder <john.kazz@gmail.com>
2024-05-20 21:24:17 -0500

    add slugify

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,47 +1,6 @@
 import { db, Products, Videos, Images } from 'astro:db'
 import productsJson from '@/muckup/total-product.json'
-import { transformAsset } from '@/lib/cloudinary/index'
-
-// export async function getProducts() {
-//   const products = await db.select().from(Products)
-//   const images = await db.select().from(Images)
-//   const videos = await db.select().from(Videos)
-//
-//   const mappedProduct = []
-//
-//   for (const product of products) {
-//     const imagesForProduct = images.filter(
-//       (img) => img.product_id === product.id,
-//     )
-//
-//     const videoForProduct = videos.find(
-//       (video) => video.product_id === product.id,
-//     )
-//
-//     mappedProduct.push({
-//       ...product,
-//       images: imagesForProduct,
-//       video: videoForProduct,
-//     })
-//   }
-//
-//   return mappedProduct
-// }
 
 export async function getProducts() {
-  return productsJson.map((prod) => ({
-    ...prod,
-    images: prod.images.map((img) => ({
-      ...img,
-      title: prod.title,
-      url: transformAsset(img.publid_id, {
-        height: 500,
-        crop: 'scale',
-      }),
-      thumb: transformAsset(img.publid_id, {
-        height: 100,
-        crop: 'thumb',
-      }),
-    })),
-  }))
+  return productsJson
 }
