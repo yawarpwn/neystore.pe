4e2e19219b5c707438de64353676c57d4ab9dbfa
johneyder <john.kazz@gmail.com>
2024-08-11 16:31:09 -0500

    implementing hybrid render

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import sitemap from '@astrojs/sitemap'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.neystore.pe',
 	trailingSlash: 'never',
 	base: '/',
-	output: 'static',
+	output: 'hybrid',
 	compressHTML: true,
 	devToolbar: {
 		enabled: false,
@@ -34,4 +36,6 @@ export default defineConfig({
 			},
 		},
 	},
+	adapter: vercel(),
 })
+
