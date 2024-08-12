bd13c9df70ff03434cd0f50d8ae6fdd6d5f55e65
johneyder <john.kazz@gmail.com>
2024-08-12 13:48:51 -0500

    remove astro:db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,40 @@
-import { defineConfig, envField } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
-
-import db from "@astrojs/db";
+import { defineConfig, envField } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.neystore.pe',
-  trailingSlash: 'never',
-  base: '/',
-  output: 'hybrid',
-  compressHTML: true,
-  devToolbar: {
-    enabled: false
-  },
-  scopedStyleStrategy: 'attribute',
-  build: {
-    assets: '_astro'
-  },
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), icon(), sitemap(), db()],
-  experimental: {
-    env: {
-      schema: {
-        CLOUDINARY_API_SECRET: envField.string({
-          context: 'server',
-          access: 'secret'
-        })
-      }
-    }
-  },
-  adapter: vercel()
-});
+	site: 'https://www.neystore.pe',
+	trailingSlash: 'never',
+	base: '/',
+	output: 'hybrid',
+	compressHTML: true,
+	devToolbar: {
+		enabled: false,
+	},
+	scopedStyleStrategy: 'attribute',
+	build: {
+		assets: '_astro',
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		icon(),
+		sitemap(),
+	],
+	experimental: {
+		env: {
+			schema: {
+				CLOUDINARY_API_SECRET: envField.string({
+					context: 'server',
+					access: 'secret',
+				}),
+			},
+		},
+	},
+	adapter: vercel(),
+})
+
