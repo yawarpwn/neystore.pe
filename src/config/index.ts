fd0eab41fb2d6e0c6877acc2dbc2df8907805cae
johneyder <john.kazz@gmail.com>
2024-09-01 12:59:49 -0500

    add video k3

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ export { site } from './site'
 
 export const envs = {
 	CLOUDINARY_API_SECRET: env.get('CLOUDINARY_API_SECRET').required().asString(),
-	TURSO_AUTH_TOKEN: env.get('TURSO_AUTH_TOKEN').required().asString(),
-	TURSO_CONNECTION_URL: env.get('TURSO_CONNECTION_URL').required().asString(),
+	// TURSO_AUTH_TOKEN: env.get('TURSO_AUTH_TOKEN').required().asString(),
+	// TURSO_CONNECTION_URL: env.get('TURSO_CONNECTION_URL').required().asString(),
 }
