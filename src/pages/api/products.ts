04ec501f06c315010cac3fac66c119e829892fae
yawarpwn <john.kazz@gmail.com>
2024-11-12 00:35:43 -0500

    remove lowdb

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,22 +1,13 @@
 import { type APIRoute } from 'astro'
 import { type Product } from '@/types'
-import path from 'node:path'
-import { JSONFilePreset } from 'lowdb/node'
+import { ProductsModel } from '@/models/products'
 
 export const GET: APIRoute = async ({ request }) => {
-	console.log('Get Request /api/products')
-	type Data = {
-		products: Product[]
-	}
-
-	const defaultData: Data = {
-		products: [],
-	}
-
-	const JSON_PRODUCTS_PATH = path.join('src/db/db.json')
 	try {
-		const db = await JSONFilePreset<Data>(JSON_PRODUCTS_PATH, defaultData)
-		const { products } = db.data
+		const { data: products, error } = await ProductsModel.getAll()
+
+		if (error) throw error
+
 		return new Response(JSON.stringify({ products }), {
 			status: 200,
 			headers: { 'Content-Type': 'application/json' },
