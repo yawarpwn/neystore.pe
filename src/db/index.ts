f2cd3592c345ba73b9add7a952d51d7713108e0f
yawarpwn <john.kazz@gmail.com>
2024-11-11 22:28:52 -0500

    add database class

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,30 @@
-import { client } from './client'
-import { drizzle } from 'drizzle-orm/libsql'
+import fs from 'fs/promises'
+import { type Product } from '@/types'
 
-export const db = drizzle(client)
+const FILE_PATH = 'src/db/db.json'
+
+type Data = {
+	products: Product[]
+}
+
+export class Database {
+	data: Data = {
+		products: [],
+	}
+
+	constructor() {}
+
+	async write() {
+		fs.writeFile(JSON.stringify(this.data), FILE_PATH)
+	}
+
+	async finOne() {
+		this.read()
+	}
+
+	async read() {
+		const data = JSON.parse(await fs.readFile(FILE_PATH, 'utf-8'))
+		this.data = data
+		return this.data
+	}
+}
