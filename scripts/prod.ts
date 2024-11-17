9a9ca07d13079fe057c5a191ee2cd722f86daef7
yawarpwn <john.kazz@gmail.com>
2024-11-16 19:10:18 -0500

    before update

--- scripts/prod.ts
+++ scripts/prod.ts
@@ -1,14 +1,52 @@
 import fs from 'node:fs'
-import products from '../src/db/db.json'
+import data from '../src/db/db.json'
 import { transformAsset } from '../src/lib/cloudinary/index'
 import type { Product } from '../src/types'
 
-// const newProducts = products.map((product) => {
-// 	return {
-// 		...product,
-// 		createdAt: new Date().toISOString(),
-// 	}
-// })
+const TAGS = {
+	MONTESSORI: 'Montessori',
+	PSYCHOMOTRICITY: 'Psicomotricidad',
+	LANGUAGE: 'Lenguaje',
+	SENSORY: 'Sensorial',
+	STRESS_RELIEF: 'Antiestrés',
+	KEYBOARDS: 'Teclados',
+	TECHNOLOGY: 'Tecnologia',
+	TOYS: 'Juguetes',
+}
+
+const newProducts = data.products.map((product) => {
+	return {
+		id: product.id,
+		title: product.title,
+		slug: product.slug,
+		price: product.price,
+		cost: product.cost,
+		stock: product.stock,
+		ranking: product.ranking,
+		features: product.features,
+		details: product.details,
+		assets: null,
+		tags: product.tags
+			.map((tag) => {
+				const t = tag.toLowerCase()
+				if (t === 'teclados') return 'KEYBOARDS'
+				if (t === 'teclado') return 'KEYBOARDS'
+				if (t === 'keychron') return 'KEYCHRON'
+				if (t === 'lenguaje') return 'LANGUAGE'
+				if (t === 'sensorial') return 'SENSORY'
+				if (t === 'Antiestrés') return 'STRESS_RELIEF'
+				if (t === 'piscomotricidad') return 'PSYCHOMOTRICITY'
+				if (t === 'montessori') return 'MONTESSORI'
+				if (t === 'juguetes') return 'TOYS'
+				if (t === 'tecnologia') return 'TECHNOLOGY'
+				return null
+			})
+			.filter(Boolean),
+		createdAt: product.createdAt,
+	}
+})
+
+console.log(newProducts)
 
 // const newProducts = products.map((product) => {
 // 	// let tags: string[] = []
@@ -65,4 +103,4 @@ import type { Product } from '../src/types'
 
 // fs.writeFileSync('./src/muckup/products.json', JSON.stringify(newProducts, null, 2))
 
-fs.writeFileSync('./src/db/db.json', JSON.stringify(newProducts, null, 2))
+// fs.writeFileSync('./src/db/db.json', JSON.stringify(newProducts, null, 2))
