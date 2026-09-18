86702d94c1a06a47a8352c6c543017d395bd268e
yawarpwn <john.kazz@gmail.com>
2026-09-17 21:01:47 -0500

    change phone

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,7 +6,7 @@ export const site = {
 	ogImage: 'https://www.neystore.pe/og-neystore.webp',
 	description:
 		'Descubre juguetes didácticos diseñados para estimular el desarrollo cognitivo, sensorial, la comunicación, el lenguaje y la motricidad en niños. Ideales para aprender jugando.',
-	phone: '900224005',
+	phone: '973071956',
 	email: 'ventas.neystore@gmail.com',
 	address: 'Av. Maquinaria 325 - Carmen de La Legua - Reynoso ',
 	socialLinks: {
